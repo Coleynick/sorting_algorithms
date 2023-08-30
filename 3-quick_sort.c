6e43bb0ef3973f96da95cb363dc1d80e01eb4713
Coleynick <ezechidinma14@gmail.com>
2023-08-29 19:12:41 -0700

    Update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,15 +1,34 @@
 #include "sort.h"
 
 /**
-* swap - Swaps 2 %d
-* @v: First %d
-* @x: Second %d
+* quick_sort - Sorts an array of %d in ascension
+* @array: D array to be sorted
+* @size: The size of the array
 */
-void swap(int *v, int *x)
+void quick_sort(int *array, size_t size)
 {
-	int tmp = *v;
-	*v = *x;
-	*x = tmp;
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_helpa(array, 0, size - 1, size);
+}
+
+/**
+* quick_sort_helpa - Help the quick_sort algorithm.
+* @array: Array to be sortd.
+* @low: Starting indx of the partition.
+* @high: Ending indx of the partition.
+* @size: Size of the arry.
+*/
+void quick_sort_helpa(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
+		quick_sort_helpa(array, low, pivot - 1, size);
+		quick_sort_helpa(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -22,56 +41,33 @@ void swap(int *v, int *x)
 */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int pivt = array[high];
-	int a = low - 1;
+	int pivot, a, j;
 
-	for (int j = low; j <= high - 1; j++)
+	pivot = array[high];
+	a = low - 1;
+
+	for (j = low; j <= high - 1; j++)
 	{
-		if (array[j] <= pivt)
+		if (array[j] < pivot)
 		{
 			a++;
-			if (a != j)
-			{
-				swap(&array[a], &array[j]);
-				print_array(array, size);
-			}
+			swap(&array[a], &array[j]);
+			print_array(array, size);
 		}
 	}
-	if (a + 1 != high)
-	{
-		swap(&array[a + 1], &array[high]);
-		print_array(array, size);
-	}
+	swap(&array[a + 1], &array[high]);
+	print_array(array, size);
 	return (a + 1);
 }
 
 /**
-* quick_sort_helpa - Help the quick_sort algorithm.
-* @array: Array to be sortd.
-* @low: Starting indx of the partition.
-* @high: Ending indx of the partition.
-* @size: Size of the arry.
+* swap - Swaps 2 %d
+* @v: First %d
+* @x: Second %d
 */
-void quick_sort_helpa(int *array, int low, int high, size_t size)
+void swap(int *v, int *x)
 {
-	if (low < high)
-	{
-		int pi = lomuto_partition(array, low, high, size);
-
-		quick_sort_helpa(array, low, pi - 1, size);
-		quick_sort_helpa(array, pi + 1, high, size);
-	}
-}
-
-/**
-* quick_sort - Sorts an array of integers using the func
-* @array: Array to be sorted
-* @size: Size of the array
-*/
-void quick_sort(int *array, size_t size)
-{
-	if (!array || size <= 1)
-		return;
-
-	quick_sort_helpa(array, 0, size - 1, size);
+	int temp = *v;
+	*v = *x;
+	*x = temp;
 }
